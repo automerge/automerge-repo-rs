02daef359296fac46db351223909e2005811024f
gterzian <2792687+gterzian@users.noreply.github.com>
2023-05-15 17:27:46 +0800

    fix tets and example

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,11 +1,14 @@
+use automerge::transaction::Transactable;
+use automerge::ReadDoc;
 use automerge_repo::{NetworkAdapter, NetworkError, NetworkEvent, NetworkMessage, Repo, RepoId};
 use core::pin::Pin;
 use futures::sink::Sink;
 use futures::stream::Stream;
 use futures::task::{Context, Poll, Waker};
 use parking_lot::Mutex;
-use std::collections::VecDeque;
+use std::collections::{HashMap, VecDeque};
 use std::sync::Arc;
+use tokio::runtime::Handle;
 use tokio::sync::mpsc::{channel, Sender};
 
 #[derive(Debug, Clone)]
@@ -30,6 +33,21 @@ impl Network<NetworkMessage> {
             sender,
             sink_waker: sink_waker.clone(),
         }
+    }
+
+    pub fn receive_incoming(&self, event: NetworkEvent) {
+        self.buffer.lock().push_back(event);
+        if let Some(waker) = self.stream_waker.lock().take() {
+            waker.wake();
+        }
+    }
+
+    pub fn take_outgoing(&self) -> NetworkMessage {
+        let message = self.outgoing.lock().pop_front().unwrap();
+        if let Some(waker) = self.sink_waker.lock().take() {
+            waker.wake();
+        }
+        message
     }
 }
 
@@ -62,6 +80,7 @@ impl Sink<NetworkMessage> for Network<NetworkMessage> {
         let repo_id = match item {
             NetworkMessage::Sync { from_repo_id, .. } => from_repo_id.clone(),
         };
+
         self.outgoing.lock().push_back(item);
         if self.sender.blocking_send(repo_id).is_err() {
             return Err(NetworkError::Error);
@@ -92,13 +111,134 @@ impl NetworkAdapter for Network<NetworkMessage> {}
 fn test_repo_stop() {
     let (sender, _network_receiver) = channel(1);
 
+    // Create the network adapter.
+    let network = Network::new(sender.clone());
+
     // Create the repo.
-    let repo = Repo::new(Network::new(sender.clone()), None);
+    let repo = Repo::new(network, None);
 
     // Run the repo in the background.
     let repo_handle = repo.run();
 
-    // All collections and doc handles have been dropped,
-    // repo should stop running.
+    // Stop the repo.
     repo_handle.stop().unwrap();
+}
+
+#[test]
+fn test_simple_sync() {
+    let (sender, mut network_receiver) = channel(1);
+    let mut repo_handles = vec![];
+    let mut documents = vec![];
+    let mut docs_to_sync = HashMap::new();
+    let mut peers = HashMap::new();
+
+    for _ in 1..10 {
+        // Create the network adapter.
+        let network = Network::new(sender.clone());
+
+        // Create the repo.
+        let repo = Repo::new(network.clone(), None);
+        let mut repo_handle = repo.run();
+
+        // Create a document.
+        let mut doc_handle = repo_handle.new_document();
+        doc_handle.with_doc_mut(|doc| {
+            doc.put(
+                automerge::ROOT,
+                "repo_id",
+                format!("{}", repo_handle.get_repo_id()),
+            )
+            .expect("Failed to change the document.");
+            doc.commit();
+        });
+
+        peers.insert(*repo_handle.get_repo_id(), network);
+        documents.push(doc_handle);
+
+        repo_handles.push(repo_handle);
+    }
+
+    // We want each repo to sync the documents created by all other repos.
+    for repo_handle in repo_handles.iter_mut() {
+        let mut doc_handles = vec![];
+        let repo_id = *repo_handle.get_repo_id();
+        for doc_handle in documents.iter() {
+            let doc_id = doc_handle.document_id();
+            if doc_id.get_repo_id() == &repo_id {
+                continue;
+            }
+            doc_handles.push(repo_handle.bootstrap_document_from_id(None, doc_id));
+        }
+        docs_to_sync.insert(repo_id, doc_handles);
+    }
+
+    let (done_sync_sender, mut done_sync_receiver) = channel(1);
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+    rt.spawn(async move {
+        Handle::current().spawn_blocking(move || {
+            // Keep looping until all docs are in sync.
+            let mut synced = 0;
+            let total_expected = docs_to_sync
+                .iter()
+                .fold(0, |acc, (_, docs)| acc + docs.len());
+            loop {
+                if synced == total_expected {
+                    done_sync_sender.blocking_send(()).unwrap();
+                    return;
+                }
+                for (_, doc_handles) in docs_to_sync.iter_mut() {
+                    for doc_handle in doc_handles {
+                        let expected_repo_id =
+                            format!("{}", doc_handle.document_id().get_repo_id());
+                        doc_handle.with_doc_mut(|doc| {
+                            let value = doc.get(automerge::ROOT, "repo_id");
+                            if value.unwrap().unwrap().0.to_str().unwrap() == expected_repo_id {
+                                synced = synced + 1;
+                            }
+                        });
+                    }
+                }
+            }
+        });
+
+        // A router of network messages.
+        loop {
+            tokio::select! {
+               outgoing_repo = network_receiver.recv() => {
+                   let incoming = {
+                       let peer = peers.get_mut(&outgoing_repo.unwrap()).unwrap();
+                       peer.take_outgoing()
+                   };
+                   match incoming {
+                       NetworkMessage::Sync {
+                           from_repo_id,
+                           to_repo_id,
+                           document_id,
+                           message,
+                       } => {
+                           let peer = peers.get_mut(&to_repo_id).unwrap();
+                           peer.receive_incoming(NetworkEvent::Sync {
+                               from_repo_id,
+                               to_repo_id,
+                               document_id,
+                               message,
+                           });
+                       }
+                   }
+               },
+            }
+        }
+    });
+
+    done_sync_receiver.blocking_recv().unwrap();
+
+    // Drop all doc handles:
+    // repo should stop running.
+    drop(documents);
+    for handle in repo_handles.into_iter() {
+        handle.stop().unwrap();
+    }
 }
