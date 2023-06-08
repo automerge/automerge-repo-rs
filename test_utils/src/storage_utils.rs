98490356d1bd3cfcb850748bd34ef85da29f7be7
gterzian <2792687+gterzian@users.noreply.github.com>
2023-06-08 17:21:56 +0800

    test document is saved before shutdown completes

--- test_utils/src/storage_utils.rs
+++ test_utils/src/storage_utils.rs
@@ -2,7 +2,7 @@ use automerge_repo::{DocumentId, Storage, StorageError};
 use futures::future::TryFutureExt;
 use futures::Future;
 use parking_lot::Mutex;
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::marker::Unpin;
 use std::sync::Arc;
 use tokio::sync::mpsc::{channel, Sender};
@@ -71,6 +71,7 @@ enum StorageRequest {
     Append(DocumentId, Vec<u8>, OneShot<()>),
     Compact(DocumentId, Vec<u8>, OneShot<()>),
     ListAll(OneShot<Vec<DocumentId>>),
+    ProcessNextResult,
 }
 
 #[derive(Clone, Debug)]
@@ -79,39 +80,77 @@ pub struct AsyncInMemoryStorage {
 }
 
 impl AsyncInMemoryStorage {
-    pub fn new(mut documents: HashMap<DocumentId, Vec<u8>>) -> Self {
+    pub fn new(mut documents: HashMap<DocumentId, Vec<u8>>, with_step: bool) -> Self {
         let (doc_request_sender, mut doc_request_receiver) = channel::<StorageRequest>(1);
+        let mut results = VecDeque::new();
+        let mut can_send_result = false;
         tokio::spawn(async move {
-            loop {
-                while let Some(request) = doc_request_receiver.recv().await {
-                    match request {
-                        StorageRequest::ListAll(sender) => {
-                            let result = documents.keys().cloned().collect();
+            while let Some(request) = doc_request_receiver.recv().await {
+                match request {
+                    StorageRequest::ListAll(sender) => {
+                        let result = documents.keys().cloned().collect();
+                        let (tx, rx) = oneshot();
+                        results.push_back(tx);
+                        tokio::spawn(async move {
+                            rx.await.unwrap();
                             let _ = sender.send(result);
-                        }
-                        StorageRequest::Load(doc_id, sender) => {
-                            let result = documents.get(&doc_id).cloned();
+                        });
+                    }
+                    StorageRequest::Load(doc_id, sender) => {
+                        let result = documents.get(&doc_id).cloned();
+                        let (tx, rx) = oneshot();
+                        results.push_back(tx);
+                        tokio::spawn(async move {
+                            rx.await.unwrap();
                             let _ = sender.send(result);
-                        }
-                        StorageRequest::Append(doc_id, mut data, sender) => {
-                            let entry = documents.entry(doc_id).or_insert_with(Default::default);
-                            entry.append(&mut data);
+                        });
+                    }
+                    StorageRequest::Append(doc_id, mut data, sender) => {
+                        let entry = documents.entry(doc_id).or_insert_with(Default::default);
+                        entry.append(&mut data);
+                        let (tx, rx) = oneshot();
+                        results.push_back(tx);
+                        tokio::spawn(async move {
+                            rx.await.unwrap();
                             let _ = sender.send(());
-                        }
-                        StorageRequest::Compact(doc_id, data, sender) => {
-                            let _entry = documents
-                                .entry(doc_id)
-                                .and_modify(|entry| *entry = data)
-                                .or_insert_with(Default::default);
+                        });
+                    }
+                    StorageRequest::Compact(doc_id, data, sender) => {
+                        let _entry = documents
+                            .entry(doc_id)
+                            .and_modify(|entry| *entry = data)
+                            .or_insert_with(Default::default);
+                        let (tx, rx) = oneshot();
+                        results.push_back(tx);
+                        tokio::spawn(async move {
+                            rx.await.unwrap();
                             let _ = sender.send(());
+                        });
+                    }
+                    StorageRequest::ProcessNextResult => {
+                        if let Some(sender) = results.pop_front() {
+                            let _ = sender.send(());
+                        } else {
+                            can_send_result = true;
                         }
                     }
+                }
+                if !with_step || can_send_result {
+                    let sender: OneShot<()> = results.pop_front().unwrap();
+                    let _ = sender.send(());
                 }
             }
         });
         AsyncInMemoryStorage {
             chan: doc_request_sender,
         }
+    }
+
+    pub async fn process_next_result(&self) {
+        self.chan
+            .send(StorageRequest::ProcessNextResult)
+            .await
+            .unwrap();
     }
 }
 
--- tests/document_save.rs
+++ tests/document_save.rs
@@ -15,7 +15,7 @@ fn test_simple_save() {
 
     let (done_sync_sender, mut done_sync_receiver) = channel(1);
     rt.spawn(async move {
-        let storage = AsyncInMemoryStorage::new(Default::default());
+        let storage = AsyncInMemoryStorage::new(Default::default(), false);
         let (expected_value, document_id) = {
             // Create one repo.
             let repo = Repo::new(None, Box::new(storage.clone()));
@@ -74,6 +74,58 @@ fn test_simple_save() {
 }
 
 #[test]
+fn test_save_on_shutdown() {
+    let rt = tokio::runtime::Builder::new_multi_thread()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    let (done_sync_sender, mut done_sync_receiver) = channel(1);
+    rt.spawn(async move {
+        // Step by step storage.
+        let storage = AsyncInMemoryStorage::new(Default::default(), true);
+        // Create one repo.
+        let repo = Repo::new(None, Box::new(storage.clone()));
+        let repo_handle = repo.run();
+
+        // Create a document for one repo.
+        let mut document_handle = repo_handle.new_document();
+
+        // Edit the document.
+        let expected_value = format!("{}", repo_handle.get_repo_id());
+        document_handle.with_doc_mut(|doc| {
+            doc.put(automerge::ROOT, "repo_id", expected_value.clone())
+                .expect("Failed to change the document.");
+            doc.commit();
+        });
+        drop(document_handle);
+
+        use tokio::sync::oneshot::channel as oneshot;
+
+        let (tx, rx) = oneshot();
+
+        tokio::spawn(async move {
+            rx.await.unwrap();
+            // Allow storage to save.
+            storage.process_next_result().await;
+        });
+
+        // Shut down the repo.
+        tokio::task::spawn_blocking(|| {
+            // Not completely deterministic, but good enough.
+            tx.send(()).unwrap();
+            repo_handle.stop().unwrap();
+        })
+        .await
+        .unwrap();
+
+        done_sync_sender.send(()).await.unwrap();
+    });
+
+    done_sync_receiver.blocking_recv().unwrap();
+}
+
+#[test]
 fn test_multiple_save() {
     let rt = tokio::runtime::Builder::new_multi_thread()
         .enable_all()
@@ -82,7 +134,7 @@ fn test_multiple_save() {
 
     let (done_sync_sender, mut done_sync_receiver) = channel(1);
     rt.spawn(async move {
-        let storage = AsyncInMemoryStorage::new(Default::default());
+        let storage = AsyncInMemoryStorage::new(Default::default(), false);
         let (expected_value, document_id) = {
             // Create one repo.
             let repo = Repo::new(None, Box::new(storage.clone()));
@@ -160,7 +212,7 @@ fn test_compact_save() {
 
     let (done_sync_sender, mut done_sync_receiver) = channel(1);
     rt.spawn(async move {
-        let storage = AsyncInMemoryStorage::new(Default::default());
+        let storage = AsyncInMemoryStorage::new(Default::default(), false);
         let (expected_value, document_id) = {
             // Create one repo.
             let repo = Repo::new(None, Box::new(storage.clone()));
