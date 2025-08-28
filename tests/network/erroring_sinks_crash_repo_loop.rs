e547d13c96d1e60cfe69eabe5cb84a26075e512c
Alex Good <alex@memoryandthought.me>
2025-08-28 09:32:52 +0100

    remove the JS interop tests
    These tests only tested interop with a very old version of
    automerge-repo which is no longer supported. The library is not
    interoperable with the current version of automerge-repo so these tests
    are really just dead weight.

--- tests/network/erroring_sinks_crash_repo_loop.rs
+++ tests/network/erroring_sinks_crash_repo_loop.rs
@@ -112,6 +112,107 @@ async fn test_errored_sinks_do_not_crash_repo_loop() {
     // .expect("timed out waiting for request");
 }
 
+#[tokio::test]
+#[test_log::test]
+async fn test_errored_sinks_do_not_crash_repo_loop_on_start_send() {
+    // This test exercises a bug in the repo code where a sink which returned
+    // an error would sometimes result in the repo loop panicking. The root
+    // cause of this is that we would call `poll_close` on a sink after `poll_flush`
+    // had returned an error.
+    //
+    // Actually observing this bug requires a bit of setup as we have to orchestrate
+    // a `poll_flush` call which returns an error followed by a `poll_close` call,
+    // this is achieved by the `FailableSink` struct below.
+    //
+    // Once we've set up the failure, we can detect that the repo loop has panicked
+    // by trying to call any suspending method as they all attempt to send a message
+    // to the repo loop, which has now crashed and dropped it's end of the channel.
+
+    // Create two repos.
+    let repo_1 = Repo::new(Some("repo1".to_string()), Box::new(SimpleStorage));
+    let repo_2 = Repo::new(Some("repo2".to_string()), Box::new(SimpleStorage));
+
+    // Run the repos in the background.
+    let repo_handle_1 = repo_1.run();
+    let repo_handle_2 = repo_2.run();
+
+    let TinCans {
+        left:
+            TinCan {
+                send: left_sink,
+                recv: left_stream,
+                sink_closed: _,
+            },
+        right:
+            TinCan {
+                send: right_sink,
+                recv: right_stream,
+                sink_closed: _,
+            },
+    } = tincans();
+    let (left_sink, left_sink_failed) = FailableSink::new(left_sink);
+    let left_sink = left_sink.sink_map_err(|err| {
+        // This combinator is just used to ensure that we use SinkMapErr, which
+        // has this behavior of panicking if poll methods are called after an
+        // error is returned
+        err
+    });
+    repo_handle_1.new_remote_repo(
+        repo_handle_2.get_repo_id().clone(),
+        left_stream,
+        Box::new(left_sink),
+    );
+    repo_handle_2.new_remote_repo(
+        repo_handle_1.get_repo_id().clone(),
+        right_stream,
+        right_sink,
+    );
+
+    // wait a bit for them to connect
+    tokio::time::sleep(Duration::from_millis(100)).await;
+
+    // Create two document on repo1
+    let document_handle_1 = repo_handle_1.new_document();
+
+    // Now fail the left sink
+    left_sink_failed.store(true, std::sync::atomic::Ordering::Relaxed);
+
+    // Edit the document.
+    document_handle_1.with_doc_mut(|doc| {
+        let mut tx = doc.transaction();
+        tx.put(
+            automerge::ROOT,
+            "repo_id",
+            format!("{}", repo_handle_1.get_repo_id()),
+        )
+        .expect("Failed to change the document.");
+        tx.commit();
+    });
+
+    // wait a bit for messages to go out
+    tokio::time::sleep(Duration::from_millis(100)).await;
+
+    // Check that we are still able to communicate with the repo loop. This will
+    // panic with a SendError if the repo loop has crashed
+    let _ = tokio::time::timeout(
+        Duration::from_millis(100),
+        repo_handle_1.peer_doc_state(
+            repo_handle_2.get_repo_id().clone(),
+            document_handle_1.document_id(),
+        ),
+    )
+    .await
+    .expect("timed out waiting for peer doc state");
+
+    // // Now try and retrieve doc 2 on repo 1
+    // let _document_handle_2 = tokio::time::timeout(
+    //     Duration::from_millis(1000),
+    //     repo_handle_1.request_document(document_handle_2.document_id()),
+    // )
+    // .await
+    // .expect("timed out waiting for request");
+}
+
 // The purpose of this stream is to expose a particular sequence of events:
 //
 // * The repo calls poll_flush on the sink
